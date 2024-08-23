46b98844bfe4e376aac3e49f4b2cd5b8f37db51f
VARUN <varun.pillai@bi3technologies.com>
2024-08-23 13:15:39 +0530

    removed the git token

--- REPO_GCP_INTEGRATION.py
+++ REPO_GCP_INTEGRATION.py
@@ -7,7 +7,7 @@ from google.cloud import bigquery
 repo_owner = "PILLAI9"
 repo_name = "Taizen"
 download_dir = r"/github/workspace/Downloads_GIT"
-github_token = "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"  # Replace with your GitHub token
+github_token = ""  # Replace with your GitHub token
 
 # Google Cloud credentials
 # os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = r'C:\\Users\\VarunPillai\\Pictures\\Screenshots\\Python Scripts\\Git_To_GCB\\pilot-project-433005-0366e733b805.json'
